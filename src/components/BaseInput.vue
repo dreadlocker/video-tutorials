e967c5a4ae4a753ca6ffc644093b64ac33ebd948
Svetozar <svetozariliev13@gmail.com>
2020-05-23 01:40:06 +0300

    BaseInput created

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label class="label">{{input.label}}</label>
+
+    <textarea
+      v-if="input.tag === 'textarea'"
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+    ></textarea>
+
+    <input
+      v-else
+      :type="input.type"
+      :placeholder="input.placeholder"
+      v-model="input.value"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    input: {
+      type: Object,
+      required: true
+    },
+  }
+};
+</script>
