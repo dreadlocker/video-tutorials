b24e2252efd5dee3994da6a213f9451b9067a917
Svetozar <svetozariliev13@gmail.com>
2020-05-22 23:05:15 +0300

    Created BaseHeading

---
+++ src/components/BaseHeading.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{text}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseHeading",
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+  }
+};
+</script>
