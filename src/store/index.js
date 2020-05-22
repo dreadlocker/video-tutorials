539186052c1c237be0cb98fc3e9ef710912c5023
Svetozar <svetozariliev13@gmail.com>
2020-05-22 23:56:11 +0300

    Added data in Vuex about available courses

--- src/store/index.js
+++ src/store/index.js
@@ -10,15 +10,59 @@ export default new Vuex.Store({
   // modules: {
   // }
   state: {
-    isUserLoggedIn: false,
-    isAdminLoggedIn: false,
+    is_user_logged_in: false,
+    is_admin_logged_In: false,
+    courses: [
+      {
+        id: 1,
+        imageSrc: "",
+        name: "React",
+        info: "Lorem Ipsum",
+        usersEnrolled: 2
+      },
+      {
+        id: 4,
+        imageSrc: "",
+        name: "Angular",
+        info: "Lorem Ipsum",
+        usersEnrolled: 1
+      },
+      {
+        id: 3,
+        imageSrc: "",
+        name: "Symfony",
+        info: "Lorem Ipsum",
+        usersEnrolled: 0
+      },
+      {
+        id: 5,
+        imageSrc: "",
+        name: "Sprint MVC",
+        info: "Lorem Ipsum",
+        usersEnrolled: 0
+      },
+      {
+        id: 2,
+        imageSrc: "",
+        name: "ASP .NET",
+        info: "Lorem Ipsum",
+        usersEnrolled: 0
+      },
+      {
+        id: 6,
+        imageSrc: "",
+        name: "Express",
+        info: "Lorem Ipsum",
+        usersEnrolled: 3
+      },
+    ],
   },
   mutations: {
     [types.IS_USER_LOGGED_IN]: (state, payload) => {
-      state.isUserLoggedIn = payload;
+      state.is_user_logged_in = payload;
     },
     [types.IS_ADMIN_LOGGED_IN]: (state, payload) => {
-      state.isAdminLoggedIn = payload;
+      state.is_admin_logged_In = payload;
     },
   },
   actions: {
