f98229237908b959bc711f7409ae7f198f922207
Svetozar <svetozariliev13@gmail.com>
2020-05-22 19:09:25 +0300

    Added routes guard in src\main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,24 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+const allowedRoutes = ["Home", "Login", "Register"];
+const userRoutes = ["AllCourses", "CourseDetails", "PlayVideo"];
+const adminRoutes = ["CreateCourse", "EditCourse", "LecturePanel"];
+
+router.beforeEach((to, from, next) => {
+  const isAllowed = allowedRoutes.some(routeName => routeName === to.name);
+  if(isAllowed) return next();
+  
+  // TODO PlayVideo -	Users (logged in and enrolled in the course) 
+  const user = userRoutes.some(routeName => routeName === to.name) && store.state.isUserLoggedIn;
+  if(user) return next();
+
+  const admin = adminRoutes.some(routeName => routeName === to.name) && store.state.isAdminLoggedIn;
+  if(admin) return next();
+
+  next(from.path);
+});
+
 new Vue({
   router,
   store,
