2140ac14d06d33cff98c6091d703f8963e27b36a
Svetozar <svetozariliev13@gmail.com>
2020-05-23 01:39:34 +0300

    BaseButton created

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    @click="onClick"
+    :type="button.type"
+    :class="button.classes"
+    class="btn"
+  >{{button.text}}</button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    button: {
+      type: Object,
+      required: true
+    },
+    onClick: {
+      type: Function,
+      required: true
+    },
+  }
+};
+</script>
