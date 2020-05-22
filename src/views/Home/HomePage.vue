9254dfadbbd5165c9abc652ce93b4fe87db75455
Svetozar <svetozariliev13@gmail.com>
2020-05-23 00:09:52 +0300

    Main HomePage shows only the page when user and admin are not logged in

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    HomePage
+    <Default v-if="!is_user_logged_in && !is_admin_logged_In" />
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import Default from "./Default/Default.vue";
+
 export default {
-  name: "HomePage"
+  name: "HomePage",
+  components: {
+    Default,
+  },
+  computed: {
+    ...mapState({
+      is_user_logged_in: state => state.is_user_logged_in,
+      is_admin_logged_In: state => state.is_admin_logged_In,
+      // courses: state => state.courses,
+    }),
+    // coursesSorted() {
+    //   // TODO
+    //   // if(this.is_user_logged_in) return courses
+    //   // TODO
+    //   // if(this.is_admin_logged_In) return courses
+    //   return this.courses.sort((a, b) => b.usersEnrolled - a.usersEnrolled).slice(0, 3)
+    // }
+  },
 };
 </script>
