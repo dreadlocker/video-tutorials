e6678e289129d59b3614ee33b6566dadd3cb1b7c
Svetozar <svetozariliev13@gmail.com>
2020-05-22 17:24:50 +0300

    Added store construction and tested vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
+// import * as types from "./types";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
+  // modules: {
+  // }
   state: {
+  // modules: {
+  // }
+    // beers_array: 1
   },
   mutations: {
+    // [types.BEERS_ARRAY]: (state, payload) => {
+    //   state.berrsArray = payload;
+    // }
   },
   actions: {
+    // [types.ACTION_BEERS_ARRAY]({ commit }, payload) {
+    //   commit(types.BEERS_ARRAY, payload);
+    // }
   },
-  modules: {
-  }
 });
---
+++ src/store/types.js
@@ -0,0 +1,5 @@
+// TYPES_NODE
+// export const BEERS_ARRAY = "store/BEERS_ARRAY";
+
+// ACTIONS_NODE
+// export const ACTION_BEERS_ARRAY = "store/ACTION_BEERS_ARRAY";
