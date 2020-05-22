64aa52e4dc5910ee82ba4e7bdf33d1e9834abb97
Svetozar <svetozariliev13@gmail.com>
2020-05-22 23:34:44 +0300

    Created BaseCourseWindow

---
+++ src/components/BaseCourseWindow.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="course-holder">
+    <!-- I don't have courses images, so I'll show a div instead of img tag -->
+    <div class="course-image">{{course.name}} image</div>
+    <div class="course-info">
+      <div>{{course.name}}</div>
+      <div>{{course.info}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCourseWindow",
+  props: {
+    course: {
+      type: Object,
+      required: true
+    },
+  }
+};
+</script>
