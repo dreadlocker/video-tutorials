f04c9ecd70ace04576a64dfd7615be7ffb1f30f8
Svetozar <svetozariliev13@gmail.com>
2020-05-23 13:06:41 +0300

    Added more funcionality

--- src/components/BaseCourseWindow.vue
+++ src/components/BaseCourseWindow.vue
@@ -1,21 +1,47 @@
 <template>
-  <div class="course-holder">
-    <!-- I don't have courses images, so I'll show a div instead of img tag -->
-    <div class="course-image">{{course.name}} image</div>
-    <div class="course-info">
-      <div>{{course.name}}</div>
-      <div>{{course.info}}</div>
+  <div>
+    <div
+      :class="{'course-holder-margin': button === undefined}"
+      class="course-holder"
+    >
+      <!-- I don't have courses images, so I'll show a div instead of img tag -->
+      <div class="course-image">{{course.name}} image</div>
+      <div class="course-info">
+        <div>{{course.name}}</div>
+        <div>{{course.info}}</div>
+      </div>
+    </div>
+    
+    <div
+      v-if="button !== undefined"
+      class="course-button-holder course-holder-margin"
+    >
+      <BaseButton
+        :button="button"
+        :onClick="onClick"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import BaseButton from "@/components/BaseButton.vue";
+
 export default {
   name: "BaseCourseWindow",
+  components: {
+    BaseButton,
+  },
   props: {
     course: {
       type: Object,
       required: true
+    },
+    button: {
+      type: Object,
+    },
+    onClick: {
+      type: Function,
     },
   }
 };
