be555d96c049758b1fd72f82e6d507cd9320818c
Svetozar <svetozariliev13@gmail.com>
2020-05-23 13:10:21 +0300

    User HomePage skeleton is done

---
+++ src/views/Home/User_logged_in/CoursesSearch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <BaseInput
+      :input="input"
+      :button="button"
+      :onClick="onClick"
+    />
+  </div>
+</template>
+
+<script>
+import BaseInput from "@/components/BaseInput.vue";
+
+export default {
+  name: "CoursesSearch",
+  components: {
+    BaseInput,
+  },
+  props: {
+    input: {
+      type: Object,
+      required: true
+    },
+    button: {
+      type: Object,
+      required: true
+    },
+    onClick: {
+      type: Function,
+      required: true
+    },
+  }
+};
+</script>
---
+++ src/views/Home/User_logged_in/HomePage.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <BaseHeading :text="contentHeaderText" />
+    <!-- TODO CoursesSearch FUNCTIONALITY NOT DONE -->
+    <CoursesSearch
+      :input="input"
+      :button="searchButton"
+      :onClick="courseSearch"
+    />
+    
+    <div class="content">
+      <BaseCourseWindow
+        v-for="course in coursesSorted"
+        :key="course.id"
+        :course="course"
+        :button="coursesButton"
+        :onClick="showCourse"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import CoursesSearch from "./CoursesSearch.vue";
+import BaseHeading from "@/components/BaseHeading.vue";
+import BaseCourseWindow from "@/components/BaseCourseWindow.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    CoursesSearch,
+    BaseHeading,
+    BaseCourseWindow,
+  },
+  computed: {
+    ...mapState({
+      courses: state => state.courses,
+    }),
+    coursesSorted() {
+      const coursesCopy = [...this.courses];
+      return coursesCopy.sort((a, b) => a.id - b.id);
+    }
+  },
+  data() {
+    return {
+      contentHeaderText: "Courses",
+      coursesButton: {
+        type: "button",
+        text: "Details",
+      },
+      input: {
+        label: "Search: ",
+        type: "text",
+        placeholder: "",
+        value: "",
+      },
+      searchButton: {
+        type: "button",
+        classes: "",
+        text: "Search",
+      },
+    };
+  },
+  methods: {
+    courseSearch() {
+      // console.log("courseSearch");
+    },
+    showCourse() {
+      // console.log("showCourse");
+    },
+  }
+};
+</script>
