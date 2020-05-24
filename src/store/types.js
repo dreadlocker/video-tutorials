77d8684e0cdc8422f0b78398877d2bf29fd14891
Svetozar <svetozariliev13@gmail.com>
2020-05-24 11:45:10 +0300

    Added mutation and action for courses

--- src/store/types.js
+++ src/store/types.js
@@ -1,11 +1,13 @@
 // TYPES_NODE
 export const IS_USER_LOGGED_IN = "store/IS_USER_LOGGED_IN";
 export const IS_ADMIN_LOGGED_IN = "store/IS_ADMIN_LOGGED_IN";
+export const UPDATE_COURSES = "store/UPDATE_COURSES";
 export const SAVE_USERS = "store/SAVE_USERS";
 export const CURRENT_USER = "store/CURRENT_USER";
 
 // ACTIONS_NODE
 export const ACTION_IS_USER_LOGGED_IN = "store/ACTION_IS_USER_LOGGED_IN";
 export const ACTION_IS_ADMIN_LOGGED_IN = "store/ACTION_IS_ADMIN_LOGGED_IN";
+export const ACTION_UPDATE_COURSES = "store/ACTION_UPDATE_COURSES";
 export const ACTION_SAVE_USERS = "store/ACTION_SAVE_USERS";
 export const ACTION_CURRENT_USER = "store/ACTION_CURRENT_USER";
