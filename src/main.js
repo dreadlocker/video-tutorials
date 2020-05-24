0828c1115e85334b316396d36ec284591c4c054a
Svetozar <svetozariliev13@gmail.com>
2020-05-24 17:18:26 +0300

    Changes added to route guards for when Admin is logged in

--- src/main.js
+++ src/main.js
@@ -11,23 +11,24 @@ const adminRoutes = ["Home", "CreateCourse", "EditCourse", "LecturePanel"];
 
 // TODO DA PROVERQ DO KOI ROUTES MOJE DA SE STIGA KOGATO NIKOI NE E LOGNAT, KOGATO USER E LOGNAT I KOGATO ADMIN E LOGNAT
 router.beforeEach((to, from, next) => {
+  const state = store.state;
+  const userName = state.current_user.username;
   const isNooneLogged = 
     allowedRoutes.some(routeName => routeName === to.name)
-    && !(store.state.is_user_logged_in)
-    && !(store.state.is_admin_logged_In);
+    && userName === undefined;
   if(isNooneLogged) return next();
   
   // TODO PlayVideo -	Users (logged in and enrolled in the course) 
-  const isUserLogged = userRoutes.some(routeName => routeName === to.name) && store.state.is_user_logged_in;
+  const isUserLogged = userRoutes.some(routeName => routeName === to.name) && userName !== "Admin";
   if (to.path.includes("/course-details/")) {
     const courseId = Number(to.params.id);
-    const courses = store.state.courses;
+    const courses = state.courses;
     const course = courses.find(course => course.id === courseId);
     if (course === undefined) return next(from.path);
   }
   if(isUserLogged) return next();
-
-  const isAdminLogged = adminRoutes.some(routeName => routeName === to.name) && store.state.is_admin_logged_In;
+  
+  const isAdminLogged = adminRoutes.some(routeName => routeName === to.name) && userName === "Admin";
   if(isAdminLogged) return next();
 
   next(from.path);
