05ab7b8bdc22a5e7b6e51b4adf5b2e064947aec3
Svetozar <svetozariliev13@gmail.com>
2020-05-24 17:20:16 +0300

    Created Admin HomePage skeleton

---
+++ src/views/Home/Admin_logged_in/Course.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="admin-course-holder">
+    <div>{{course.name}}</div>
+    <div>
+      <BaseButton
+        :button="editCourseButton"
+        :onClick="editCourse"
+        :argument="course.id"
+      />
+      <BaseButton
+        :button="addLectureButton"
+        :onClick="addLecture"
+        :argument="course.id"
+      />
+      <span class="lectures-in-course">{{lecturesInCourse}}</span>
+      <span class="lectures-count primary">{{lecturesCount}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseButton from "@/components/BaseButton.vue";
+
+export default {
+  name: "Course",
+  props: {
+    course: {
+      type: Object,
+      required: true,
+    },
+    editCourseButton: {
+      type: Object,
+      required: true,
+    },
+    editCourse: {
+      type: Function,
+      required: true,
+    },
+    addLectureButton: {
+      type: Object,
+      required: true,
+    },
+    addLecture: {
+      type: Function,
+      required: true,
+    },
+    lecturesInCourse: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    BaseButton,
+    // BaseCourseWindow,
+  },
+  computed: {
+    lecturesCount() {
+      return this.course.lectures.length;
+    }
+  }
+};
+</script>
---
+++ src/views/Home/Admin_logged_in/HomePage.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <BaseButton
+      :button="createCourseButton"
+      :onClick="createCourse"
+    />
+      <!-- :argument="course.id" -->
+
+    <BaseHeading
+      :text="contentHeaderText"
+      :classes="headingClasses"
+    />
+
+    <div class="admin-courses-holder">
+      <Course
+        v-for="course in courses"
+        :key="course.id"
+        :course="course"
+        :editCourseButton="editCourseButton"
+        :editCourse="editCourse"
+        :addLectureButton="addLectureButton"
+        :addLecture="addLecture"
+        :lecturesInCourse="lecturesInCourse"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import BaseHeading from "@/components/BaseHeading.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import Course from "./Course.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    BaseHeading,
+    BaseButton,
+    Course,
+  },
+  computed: {
+    ...mapState({
+      courses: state => state.courses,
+    }),
+    coursesSorted() {
+      const coursesCopy = [...this.courses];
+      return coursesCopy.sort((a, b) => b.usersEnrolled - a.usersEnrolled).slice(0, 3);
+    }
+  },
+  data() {
+    return {
+      headingClasses: "text-align-center",
+      contentHeaderText: "Choose Course",
+      createCourseButton: {
+        type: "button",
+        classes: "primary",
+        text: "Create Course",
+      },
+      editCourseButton: {
+        type: "button",
+        classes: "warning",
+        text: "Edit Course",
+      },
+      addLectureButton: {
+        type: "button",
+        classes: "success",
+        text: "Add lecture",
+      },
+      lecturesInCourse: "Lectures in course"
+    };
+  },
+  methods: {
+    createCourse() {
+      console.log("createCourse() {");
+    },
+    editCourse() {
+      console.log("editCourse() {");
+    },
+    addLecture() {
+      console.log("addLecture() {");
+    },
+  }
+};
+</script>
