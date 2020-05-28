6ab8e55dcb2c57ab6854e3be584333b5ab603f24
Svetozar <svetozariliev13@gmail.com>
2020-05-28 21:40:31 +0300

    Refactor router guards

--- src/main.js
+++ src/main.js
@@ -5,33 +5,19 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
-const allowedRoutes = ["Home", "Login", "Register"];
-const userRoutes = ["Home", "CourseDetails", "PlayVideo"];
-const adminRoutes = ["Home", "CreateCourse", "EditCourse", "LecturePanel"];
-
-// TODO DA PROVERQ DO KOI ROUTES MOJE DA SE STIGA KOGATO NIKOI NE E LOGNAT, KOGATO USER E LOGNAT I KOGATO ADMIN E LOGNAT
 router.beforeEach((to, from, next) => {
-  const state = store.state;
-  const userName = state.current_user.username;
-  const isNooneLogged = 
-    allowedRoutes.some(routeName => routeName === to.name)
-    && !userName;
-  if(isNooneLogged) return next();
-  
-  // TODO PlayVideo -	Users (logged in and enrolled in the course) 
-  const isUserLogged = userRoutes.some(routeName => routeName === to.name) && userName !== "Admin";
-  if (to.path.includes("/course-details/")) {
-    const courseId = Number(to.params.id);
-    const courses = state.public_courses;
-    const course = courses.find(course => course.id === courseId);
-    if (!course) return next(from.path);
-  }
-  if(isUserLogged) return next();
-  
-  const isAdminLogged = adminRoutes.some(routeName => routeName === to.name) && userName === "Admin";
-  if(isAdminLogged) return next();
+  const requiresAccess = to.meta.requiresAccess;
+  const userAccess = store.state.current_user.access;
 
-  next(from.path);
+  if (requiresAccess) {
+    if (requiresAccess === userAccess) {
+      next();
+    } else {
+      next({name: "Login"});
+    }
+  } else {
+    next();
+  }
 });
 
 new Vue({
